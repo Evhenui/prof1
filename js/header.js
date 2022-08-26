ccd6197fb6059391ae9e147d3e6387c2761d2e74
Evhenui <73481852+Evhenui@users.noreply.github.com>
2022-08-27 01:19:50 +0300

    does submenu

--- js/header.js
+++ js/header.js
@@ -1,3 +1,23 @@
 export let headerProfi = function() {
-    console.log("hellow");
+    const header = document.querySelector('[data-header]');
+
+    if(header !== null) {
+        const selectLanguage = header.querySelectorAll('[data-select-language]');
+        
+        function addActiveClass(list) {
+            list.forEach((item) => {
+              item.addEventListener("click", function () {
+                if (!item.classList.contains("active")) {
+                  list.forEach(function (item) {
+                    item.classList.remove("active");
+                  });
+                  item.classList.add("active");
+                }
+              });
+            });
+        }
+        addActiveClass(selectLanguage)
+        
+    }
+    
 }
