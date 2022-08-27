5bfee1c534dbd2875a9e9e85e66277dd4054cbb6
Evhenui <73481852+Evhenui@users.noreply.github.com>
2022-08-27 18:15:19 +0300

    finish catalog

--- js/header.js
+++ js/header.js
@@ -1,23 +1,73 @@
 export let headerProfi = function() {
     const header = document.querySelector('[data-header]');
 
-    if(header !== null) {
-        const selectLanguage = header.querySelectorAll('[data-select-language]');
-        
-        function addActiveClass(list) {
-            list.forEach((item) => {
-              item.addEventListener("click", function () {
-                if (!item.classList.contains("active")) {
-                  list.forEach(function (item) {
-                    item.classList.remove("active");
-                  });
-                  item.classList.add("active");
-                }
-              });
+    function addActiveClass(list) {
+      list.forEach((item) => {
+        item.addEventListener("click", function () {
+          if (!item.classList.contains("active")) {
+            list.forEach(function (item) {
+              item.classList.remove("active");
             });
-        }
-        addActiveClass(selectLanguage)
-        
+            item.classList.add("active");
+          }
+        });
+      });
     }
-    
+    function delActiv(param) {
+      param.forEach((el) => {
+        el.classList.remove('active');
+      })
+    } 
+
+    if(header !== null) {
+        const selectLanguage = header.querySelectorAll('[data-select-language]'),
+              catalogMenu = header.querySelector('[data-catalog-menu]'),
+              menuServices = header.querySelector('[data-menu-services]');
+        //----add/remove class groop------
+        addActiveClass(selectLanguage)
+        //----catalog---------------------
+        if(catalogMenu !== null) {
+          const buttonCatalog = header.querySelector('[data-button-show-catalog]'),
+                menuItems = header.querySelectorAll('[data-catalog-menu-item]'),
+                submenuItems = header.querySelectorAll('[data-item-submenu]');
+
+          buttonCatalog.addEventListener('click', function() {
+            catalogMenu.classList.toggle('active');
+          })
+
+          window.addEventListener('click',(e)=>{
+            const click = e.composedPath().includes(catalogMenu);
+            const clickButton = e.composedPath().includes(buttonCatalog);
+            if(!click && !clickButton) {
+              catalogMenu.classList.remove('active');
+            }
+          })
+         //----open submenu---------------------
+          menuItems.forEach((item, index) => {
+            item.addEventListener("click", function () {
+              delActiv(menuItems);
+              delActiv(submenuItems);
+              item.classList.add('active');
+              submenuItems[index].classList.add('active');
+            });
+          });
+        }               
+        
+        if(menuServices !== null) {
+          const buttonOpen = header.querySelector('[data-button-services]');
+          buttonOpen.addEventListener('click', function() {
+            this.classList.toggle('active');
+            menuServices.classList.toggle('active');
+          })
+
+          window.addEventListener('click',(e)=>{
+            const click = e.composedPath().includes(menuServices);
+            const clickButton = e.composedPath().includes(buttonOpen);
+            if(!click && !clickButton) {
+              menuServices.classList.remove('active');
+              buttonOpen.classList.remove('active');
+            }
+          })
+        }  
+    }
 }
