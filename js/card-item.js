35f43f854d29259ff909f9483f4007597a410576
Evhenui <73481852+Evhenui@users.noreply.github.com>
2022-09-01 21:04:12 +0300

    finish card-item

--- js/card-item.js
+++ js/card-item.js
@@ -1,3 +1,25 @@
 export let cardItem = function() {
+    const cardItem = document.querySelector('[data-card-item-section]');
 
+    function delActiv(param) {
+        param.forEach((el) => {
+          el.classList.remove('active');
+        })
+    }
+
+    if(cardItem !== null) {
+        const items = cardItem.querySelectorAll('[data-card-color-item]'),
+              likeButton = cardItem.querySelector('[data-card-button-like]');
+
+        items.forEach((item) => {
+            item.addEventListener("click", function () {
+              delActiv(items);
+              item.classList.add('active');
+            });
+        });
+
+        likeButton.addEventListener('click', function() {
+            this.classList.toggle('active');
+        })
+    }
 }
