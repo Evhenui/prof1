a8b648231bc34e8068610c5c44f327ea27476524
Evhenui <73481852+Evhenui@users.noreply.github.com>
2022-09-01 12:12:33 +0300

    catalog fix

--- js/catalog.js
+++ js/catalog.js
@@ -1,24 +1,4 @@
 export let catalog = function() {
     const catalogSection = document.querySelector('[data-catalog-page]');
 
-    if(catalogSection !== null) {
-        const catalogItems = catalogSection.querySelectorAll('[data-catalog-item]'),
-              submenuItems = catalogSection.querySelectorAll('[data-catalog-submenu]');
-
-        function delActiv(param) {
-            param.forEach((el) => {
-              el.classList.remove('active');
-            })
-        } 
-
-        catalogItems.forEach((item, index) => {
-            item.addEventListener("click", function () {
-              delActiv(catalogItems);
-              delActiv(submenuItems);
-              item.classList.add('active');
-              submenuItems[index].classList.add('active');
-            });
-          });
-    }
-
 }
