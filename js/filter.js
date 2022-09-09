86b1a329114849f2a5a48fa7f5f1d201215a5be6
Evhenui <73481852+Evhenui@users.noreply.github.com>
2022-09-10 01:09:56 +0300

    top section nav filter

--- js/filter.js
+++ js/filter.js
@@ -1,4 +1,17 @@
 export let filter = function() {
-
+    const filterSection = document.querySelector('[data-filter]');
     
+    if(filterSection !== null) {
+        const filterSwitch = filterSection.querySelector('[data-filter-switch]'),
+              dropdownFilter = filterSection.querySelector('[data-dropdown-filter]');
+
+
+        filterSwitch.addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
+
+        dropdownFilter.addEventListener('click', function() {
+            this.classList.toggle('active');
+        })
+    }
 }
