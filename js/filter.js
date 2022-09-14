94a332d202e79f3f845bd39e75f95b84a889ca12
Evhenui <zlobnuichack@gmail.com>
2022-09-14 22:36:37 +0300

    some fix item card

--- js/filter.js
+++ js/filter.js
@@ -7,11 +7,30 @@ export let filter = function() {
               inputsCheckbox = filterSection.querySelectorAll('[data-checkbox-input]'),
               titleCheckBox = filterSection.querySelectorAll('[data-title-checkbox]'),
               filterPrice = filterSection.querySelector('[data-filter-price]'),
-              filterRange = filterSection.querySelector('[data-filter-range-title]');
+              filterRange = filterSection.querySelector('[data-filter-range-title]'),
+              navigationBar = filterSection.querySelector('[data-left-navigation-bar]'),
+              catalogIntems = filterSection.querySelector('[data-catalog-items]'),
+              scrollBarr = filterSection.querySelector('[data-scroll-bar]'),
+              emptySection = filterSection.querySelector('[data-empty-section]'),
+              buttonsChangeView = filterSection.querySelectorAll('[data-button-list-view]'),
+              productItems = filterSection.querySelectorAll('[data-product-item-change-view]');
 
+              function delActiv(param) {
+                param.forEach((el) => {
+                  el.classList.remove('active');
+                })
+              }
+
+              function delPalit(param) {
+                param.forEach((el) => {
+                  el.classList.remove('palit');
+                })
+              }
 
         filterSwitch.addEventListener('click', function() {
             this.classList.toggle('active');
+            navigationBar.classList.toggle('active');
+            catalogIntems.classList.toggle('active');
         });
 
         dropdownFilter.addEventListener('click', function() {
@@ -20,13 +39,37 @@ export let filter = function() {
 
         filterRange.addEventListener('click', function() {
             filterPrice.classList.toggle('active');
-        })
-
+        });
 
         titleCheckBox.forEach((item) => {
             item.addEventListener('click', function() {
                 this.classList.toggle('active');
           })
-          })
+        });
+
+        window.addEventListener("scroll", function () {
+            if (window.scrollY > 121) {
+                scrollBarr.classList.add('active');
+                emptySection.classList.add('active');
+            } else {
+                scrollBarr.classList.remove('active');
+                emptySection.classList.remove('active');
+            }
+        });
+
+        buttonsChangeView.forEach((item) => {
+            item.addEventListener('click', function() {
+                delActiv(buttonsChangeView);
+                item.classList.add('active');
+                if(item.classList.contains('top-navigation__button-normal')) {
+                    delPalit(productItems);
+                }else {
+                    productItems.forEach(item => {
+                        item.classList.add('palit');
+                    })
+                }
+            })
+        });
+
     }
 }
