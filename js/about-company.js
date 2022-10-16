72e271f5fc074580aef5d108c79ca0465f0c7aaf
Evhenui <zlobnuichack@gmail.com>
2022-10-16 16:24:18 +0300

    add 2 section about company

--- js/about-company.js
+++ js/about-company.js
@@ -11,27 +11,59 @@ export let aboutCompany = function() {
         const tabItemsAbout = aboutCompanyPage.querySelectorAll('[data-tab-item-about-company]'),
               tabSections = aboutCompanyPage.querySelectorAll('[data-section-about-page]'),
               scrollTab = aboutCompanyPage.querySelector('[data-scroll-tab-about]'),
-              mainSectionAbout = aboutCompanyPage.querySelector('[data-scroll-section-tab-about]');
+              mainSectionAbout = aboutCompanyPage.querySelector('[data-scroll-section-tab-about]'),
+              faqSection = aboutCompanyPage.querySelector('[data-section-about-faq]');
 
-              
-
-        tabItemsAbout.forEach(item => {
-            item.addEventListener('click', function() {
-                let currentBtn = item;
-                let tabId = currentBtn.getAttribute("data-tab");
-                let currentTub = document.querySelector(tabId);
-                delActiv(tabItemsAbout)
-                delActiv(tabSections)
-                currentBtn.classList.add("active");
-                currentTub.classList.add("active"); 
-
-                window.getComputedStyle(scrollTab).getPropertyValue('--transform');
-                scrollTab.style.setProperty('--transform', -currentTub.offsetTop + 'px'); 
-
-                window.getComputedStyle(mainSectionAbout).getPropertyValue('--height');
-                mainSectionAbout.style.setProperty('--height', currentTub.scrollHeight + 'px'); 
+        tabItemsAbout.forEach((item, index) => {
+                item.addEventListener('click', function(event) {
+                    event.preventDefault();
+                    delActiv(tabItemsAbout)
+                    delActiv(tabSections)
+                    tabItemsAbout[index].classList.add("active");
+                    tabSections[index].classList.add("active"); 
+    
+                    window.getComputedStyle(scrollTab).getPropertyValue('--transform');
+                    scrollTab.style.setProperty('--transform', -tabSections[index].offsetTop + 'px'); 
+    
+                    window.getComputedStyle(mainSectionAbout).getPropertyValue('--height');
+                    mainSectionAbout.style.setProperty('--height', tabSections[index].scrollHeight + 'px'); 
                 });
-                
         });
+
+        window.addEventListener('resize', () => {
+            tabSections.forEach((item) => {
+            
+                if(item.classList.contains('active')) {                   
+                    window.getComputedStyle(scrollTab).getPropertyValue('--transform');
+                    scrollTab.style.setProperty('--transform', -item.offsetTop + 'px'); 
+                    window.getComputedStyle(mainSectionAbout).getPropertyValue('--height');
+                    mainSectionAbout.style.setProperty('--height', item.scrollHeight + 'px'); 
+
+                   
+                }
+            })
+        });
+
+
+        if(faqSection !== null) {
+          const dropdownsHead = faqSection.querySelectorAll('[data-about-company-short-information]'),
+                dropdownsBody = faqSection.querySelectorAll('[data-about-company-full-information]');
+
+                dropdownsHead.forEach((item, index) => {
+                    item.addEventListener('click', function() {
+                        if(dropdownsHead[index].classList.contains('active')) {
+                            dropdownsHead[index].classList.remove("active");
+                            dropdownsBody[index].classList.remove("active"); 
+                            dropdownsBody[index].style.height = 0 + 'px';
+                        } else {
+                            dropdownsHead[index].classList.add("active");
+                            dropdownsBody[index].classList.add("active"); 
+                            dropdownsBody[index].style.height = dropdownsBody[index].scrollHeight + 'px'; 
+                        }
+                       
+                        
+                    });
+            });
+        }
     }
 }
